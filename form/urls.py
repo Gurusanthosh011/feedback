ed338ea8ee6fa3ec5099bbb85929d75de4da50d1
Gurusanthosh011 <santhoshsagalasanthosh@gmail.com>
2024-03-22 10:08:50 +0530

    Your commit message here

---
+++ form/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+
+from . import views
+
+app_name = 'form'
+urlpatterns = [
+    path('', views.feedback_form, name='home'),
+]
---
+++ form/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from .forms import FeedbackForm
+
+
+def feedback_form(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'thanks.html')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback_form.html', {'form': form})
